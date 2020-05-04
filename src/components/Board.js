0d8c1fefa24220fb7643ce008d50aa77449e0567
Rasim copy <rasimCOPY@gmail.com>
2020-05-04 21:45:08 +0400

    Startgame animation created

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import socketIOClient from 'socket.io-client'
 import Player from './Player';
 import MyPlayer from './MyPlayer';
@@ -18,38 +18,61 @@ import bcard from '../images/love_cards/cards_loveletter-09.svg'
 const ENDPOINT = "http://104.248.20.1:8080";
 
 const Board = () => {
+    const [handCard,setHandCard] = useState(card7)
+    const [newCard,setNewCard] = useState()
     let n_players = parseInt(localStorage.getItem("n_players"))
 
-    let deck = [card1, card1, card1, card1, card1, card2, card2, card3, card3, card4, card4, card5, card5,
-        card6, card6, card7, card8]
+    // let deck = [card1, card1, card1, card1, card1, card2, card2, card3, card3, card4, card4, card5, card5,
+    //     card6, card6, card7, card8]
 
-    console.log(deck);
+    // console.log(deck);
 
     let players = []
     for (let i = 0; i < n_players - 1; i++) {
-        players.push(<Player name={"nickname"} />)
+        players.push(<Player name={"Anar"} />)
     }
 
-    const startGame = () => {
+    const playerOneCard = ()=>{
         document.getElementsByClassName("one")[0].style.animationName = "playerOne"
-        setTimeout(() => {
-            document.getElementsByClassName("two")[0].style.animationName = "playerTwo"
-            setTimeout(() => {
-                document.getElementsByClassName("three")[0].style.animationName = "playerThree"
-                setTimeout(()=>newCardtoMe(),1000)
-            }, 1000)
-        }, 1000)
+        setTimeout(()=> document.getElementsByClassName("one")[0].style.animationName = "",3000)
+    }
 
+    const playerTwoCard = ()=>{
+        document.getElementsByClassName("two")[0].style.animationName = "playerTwo"
+        setTimeout(()=> document.getElementsByClassName("two")[0].style.animationName = "",3000)
+    }
+
+    const playerThreeCard = ()=>{
+        document.getElementsByClassName("three")[0].style.animationName = "playerThree"
+        setTimeout(()=> document.getElementsByClassName("three")[0].style.animationName = "",3000)
+    }
+
+    const myFirstCard = ()=>{
+        document.getElementsByClassName("myCard")[0].style.animationName = "myFirstCard"
+        setTimeout(() => {
+            document.getElementsByClassName("inHand")[0].style.opacity = "1"
+        }, 400)
     }
 
     const newCardtoMe = () => {
         document.getElementsByClassName("myCard")[0].style.animationName = "myCard"
         setTimeout(() => {
-            document.getElementsByClassName("myCard")[0].style.animationName = "myCardOpen"
             document.getElementsByClassName("newCard")[0].style.opacity = "1"
-        }, 1500)
+        }, 400)
+
     }
 
+
+    const startGame = () => {
+        setNewCard(card8)
+        setTimeout(()=>myFirstCard(),1000)  
+        setTimeout(()=>playerOneCard(),2000)
+        setTimeout(()=>playerTwoCard(),3000)
+        setTimeout(()=>playerThreeCard(),4000)
+        setTimeout(()=>newCardtoMe(),5000)   
+    }
+
+    
     return (
         <div>
             <div className="Board">
@@ -59,8 +82,8 @@ const Board = () => {
                 {players}
                 <MyPlayer name="Nigar" />
                 <div className="right-corner-images">
-                    <img className="inHand" src={card1} />
-                    <img className="newCard" src={card2} />
+                    <img className="inHand" src={handCard} />
+                    <img className="newCard" src={newCard} />
                 </div>
                 <img className="myCard" src={bcard} />
                 <div onClick={startGame} className="right-koloda">
