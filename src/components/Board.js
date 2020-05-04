97bcc2618fa256e3e5a0469d9c11ede3e88a159e
Rasim copy <rasimCOPY@gmail.com>
2020-05-04 17:52:47 +0400

    Cards andded and position problem fixed(Board.js)

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,6 +4,8 @@ import Player from './Player';
 import MyPlayer from './MyPlayer';
 import koloda from '../images/onboard_assets/Koloda.svg'
 import '../style/Board.css'
+import card1 from '../images/love_cards/cards_loveletter-02.svg'
+import card2 from '../images/love_cards/cards_loveletter-03.svg'
 
 const ENDPOINT = "http://104.248.20.1:8080";
 
@@ -14,12 +16,22 @@ const Board = () => {
     for (let i = 0; i < n_players - 1; i++) {
         players.push(<Player name={"nickname"} />)
     }
+
+    const cards = ()=>
+    {
+        console.log("Card paylamaq")
+    }
+
     return (
         <div>
             <div className="Board">
                 {players}
                 <MyPlayer name="Nigar" />
-                <div className="right-koloda">
+                <div className="right-corner-images">
+                    <img src={card1} />
+                    <img src={card2} />
+                </div>
+                <div onClick={cards} className="right-koloda">
                     <img src={koloda} />
                 </div>
             </div>
--- src/components/MyPlayer.js
+++ src/components/MyPlayer.js
@@ -3,14 +3,12 @@ import orangeCrown from '../images/onboard_assets/orange_crown.svg'
 import heartPink from '../images/onboard_assets/pink_heart.svg'
 import heartWhite from '../images/onboard_assets/white_heart.svg'
 import timeIcon from '../images/onboard_assets/time_icon.svg'
-import card1 from '../images/love_cards/cards_loveletter-02.svg'
-import card2 from '../images/love_cards/cards_loveletter-03.svg'
 import '../style/MyPlayer.css'
 
 const MyPlayer = () => {
 
     return (
-        <div className="player-card player-card-last">
+        <div className="myplayer-card player-card-last">
             <div className="name-hearts-container">
                 <div className="hearts-container">
                     <img src={heartPink} />
@@ -28,10 +26,6 @@ const MyPlayer = () => {
                 <div className="crown-container">
                     <img src={orangeCrown} />
                 </div>
-            </div>
-            <div className="right-corner-images">
-                <img src={card1} />
-                <img src={card2} />
             </div>
         </div>
     )
