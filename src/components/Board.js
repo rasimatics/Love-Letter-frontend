ed6df556a58fc49e3ac26104e42f8deb41a26bb8
Rasim copy <rasimCOPY@gmail.com>
2020-05-06 20:43:31 +0400

    Reloading page does not effect to the game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import socketIOClient from 'socket.io-client'
 import Player from './Player';
 import MyPlayer from './MyPlayer';
@@ -29,7 +29,7 @@ const Board = () => {
     socket.on("err", data => console.log(data))
     socket.on("join-game", data => console.log("Join", data));
     socket.on("player", data => console.log("Player", data));
-    socket.on("game", data => { localStorage.setItem("players", JSON.stringify(data.players)); setPlayers(data.players); console.log(data) });
+    socket.on("game", data => { localStorage.setItem("players", JSON.stringify(data.players)); setPlayers(data.players) });
 
     socket.emit('join-game',
         {
@@ -41,90 +41,94 @@ const Board = () => {
             }
         })
 
-    document.onkeydown = (event) => {
-        if (event.keyCode == 116)
-            event.preventDefault()
+    useEffect(() => {
+        const URL = "http://104.248.20.1:8080/api/game/" + localStorage.getItem("gid")
+        fetch(URL).then(response => response.json())
+            .then(data =>{
+                if(players.length == 0)
+                {
+                    setPlayers(data.players)
+                } 
+            })
+    },[])
+
+const playerOneCard = () => {
+    document.getElementsByClassName("one")[0].style.animationName = "playerOne"
+    setTimeout(() => document.getElementsByClassName("one")[0].style.animationName = "", 3000)
+}
+
+const playerTwoCard = () => {
+    document.getElementsByClassName("two")[0].style.animationName = "playerTwo"
+    setTimeout(() => document.getElementsByClassName("two")[0].style.animationName = "", 3000)
+}
+
+const playerThreeCard = () => {
+    document.getElementsByClassName("three")[0].style.animationName = "playerThree"
+    setTimeout(() => document.getElementsByClassName("three")[0].style.animationName = "", 3000)
+}
+
+const myFirstCard = () => {
+    document.getElementsByClassName("myCard")[0].style.animationName = "myFirstCard"
+    setTimeout(() => {
+        document.getElementsByClassName("inHand")[0].style.opacity = "1"
+    }, 400)
+}
+
+const newCardtoMe = () => {
+    document.getElementsByClassName("myCard")[0].style.animationName = "myCard"
+    setTimeout(() => {
+        document.getElementsByClassName("newCard")[0].style.opacity = "1"
+    }, 400)
+
+}
+
+
+const startGame = () => {
+    setNewCard(card8)
+    if (n_players === 4) {
+        setTimeout(() => myFirstCard(), 1000)
+        setTimeout(() => playerOneCard(), 2000)
+        setTimeout(() => playerTwoCard(), 3000)
+        setTimeout(() => playerThreeCard(), 4000)
+        setTimeout(() => newCardtoMe(), 5000)
     }
-
-    window.stop();
-
-    const playerOneCard = () => {
-        document.getElementsByClassName("one")[0].style.animationName = "playerOne"
-        setTimeout(() => document.getElementsByClassName("one")[0].style.animationName = "", 3000)
+    else if (n_players === 3) {
+        setTimeout(() => myFirstCard(), 1000)
+        setTimeout(() => playerOneCard(), 2000)
+        setTimeout(() => playerThreeCard(), 3000)
+        setTimeout(() => newCardtoMe(), 4000)
+    } else {
+        setTimeout(() => myFirstCard(), 1000)
+        setTimeout(() => playerTwoCard(), 2000)
+        setTimeout(() => newCardtoMe(), 3000)
     }
-
-    const playerTwoCard = () => {
-        document.getElementsByClassName("two")[0].style.animationName = "playerTwo"
-        setTimeout(() => document.getElementsByClassName("two")[0].style.animationName = "", 3000)
-    }
-
-    const playerThreeCard = () => {
-        document.getElementsByClassName("three")[0].style.animationName = "playerThree"
-        setTimeout(() => document.getElementsByClassName("three")[0].style.animationName = "", 3000)
-    }
-
-    const myFirstCard = () => {
-        document.getElementsByClassName("myCard")[0].style.animationName = "myFirstCard"
-        setTimeout(() => {
-            document.getElementsByClassName("inHand")[0].style.opacity = "1"
-        }, 400)
-    }
-
-    const newCardtoMe = () => {
-        document.getElementsByClassName("myCard")[0].style.animationName = "myCard"
-        setTimeout(() => {
-            document.getElementsByClassName("newCard")[0].style.opacity = "1"
-        }, 400)
-
-    }
+}
 
 
-    const startGame = () => {
-        setNewCard(card8)
-        if (n_players === 4) {
-            setTimeout(() => myFirstCard(), 1000)
-            setTimeout(() => playerOneCard(), 2000)
-            setTimeout(() => playerTwoCard(), 3000)
-            setTimeout(() => playerThreeCard(), 4000)
-            setTimeout(() => newCardtoMe(), 5000)
-        }
-        else if (n_players === 3) {
-            setTimeout(() => myFirstCard(), 1000)
-            setTimeout(() => playerOneCard(), 2000)
-            setTimeout(() => playerThreeCard(), 3000)
-            setTimeout(() => newCardtoMe(), 4000)
-        } else {
-            setTimeout(() => myFirstCard(), 1000)
-            setTimeout(() => playerTwoCard(), 2000)
-            setTimeout(() => newCardtoMe(), 3000)
-        }
-    }
+return (
+    <div>
+        <div className="Board">
+            <img className="player one" src={bcard} alt="" />
+            <img className="player two" src={bcard} alt="" />
+            <img className="player three" src={bcard} alt="" />
 
+            {players.map(player => player.id != localStorage.getItem("id") && <Player key={player.id} name={player.id} mydiscard={[]} />)}
 
-    return (
-        <div>
-            <div className="Board">
-                <img className="player one" src={bcard} alt="" />
-                <img className="player two" src={bcard} alt="" />
-                <img className="player three" src={bcard} alt="" />
+            <MyPlayer name={localStorage.getItem("id")} stars={mystars} mydiscard={[]} />
 
-                {players.map(player => player.id != localStorage.getItem("id") && <Player key={player.id} name={player.id} mydiscard={[]} />)}
+            <div className="right-corner-images">
+                <img className="inHand" src={handCard} alt="" />
+                <img className="newCard" src={newCard} alt="" />
+            </div>
 
-                <MyPlayer name={localStorage.getItem("id")} stars={mystars} mydiscard={[]} />
+            <img className="myCard" src={bcard} />
 
-                <div className="right-corner-images">
-                    <img className="inHand" src={handCard} alt="" />
-                    <img className="newCard" src={newCard} alt="" />
-                </div>
-
-                <img className="myCard" src={bcard} />
-
-                <div onClick={startGame} className="right-koloda">
-                    <img src={koloda} alt="" />
-                </div>
+            <div onClick={startGame} className="right-koloda">
+                <img src={koloda} alt="" />
             </div>
         </div>
-    )
+    </div>
+)
 }
 
 export default Board
